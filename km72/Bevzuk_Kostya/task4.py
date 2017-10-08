9b29417a2e5a6197a66d95dd25e9033a9524492b
KostiaBevz <31801208+KostiaBevz@users.noreply.github.com>
2017-10-08 19:33:33 +0300

    Поняття змінної та типу даних у Python

---
+++ km72/Bevzuk_Kostya/task4.py
@@ -0,0 +1,5 @@
+a = int (input("Enter number of students:"))
+b = int (input("Enter number of apples:"))
+Task1 = b//a
+Task2 = b%a
+print('Students have:', Task1 ,"Left in backet:", Task2)
---
+++ km72/Bevzuk_Kostya/task6.py
@@ -0,0 +1,7 @@
+a = int(input("first class:"))
+b = int(input("second class:"))
+c = int(input("third class:"))
+d = a//2+a%2
+e = b//2+b%2
+r = c//2+c%2
+print("COunt of tables:", d+e+r)
