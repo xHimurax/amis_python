6a5be8184983d57243978d41c30b9c4c72bc5642
Holodenko_Artem <31826517+Acnologiak@users.noreply.github.com>
2017-10-08 19:52:25 +0300

    Homework3

---
+++ km72/Holodenko_Artem/task1.py
@@ -0,0 +1,7 @@
+import time
+print("Ця програма знаходить суму 3 чисел.")
+a=float(input("Введіть а: "))
+b=float(input("Введіть b: "))
+c=float(input("Введіть c: "))
+print("Їхня сума=",(a+b+c))
+time.sleep(3)
