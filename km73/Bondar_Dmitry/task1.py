350fa6681a27c02759d61949f76f66bdee11a8e3
Dima Bondar <31803588+dmitrypm@users.noreply.github.com>
2017-10-08 17:22:37 +0300

    Homework
    Домашне завдання 3. Поняття змінної та типу даних у python

---
+++ km73/Bondar_Dmitry/task1.py
@@ -0,0 +1,5 @@
+﻿while True:
+  number1 = float(input("Введіть перше число:"))
+  number2 = float(input("Введіть друге число:"))
+  number3 = float(input("Введіть третє число:"))
+  print(number1+number2+number3)
---
+++ km73/Bondar_Dmitry/task7.py
@@ -0,0 +1,6 @@
+﻿while True:
+  min = int(input("Введіть кількість хвилин:"))
+  hour = min//60
+  min %= 60
+  hour %= 24
+  print("Години -" , int(hour) , "Хвилини -" , int(min))
