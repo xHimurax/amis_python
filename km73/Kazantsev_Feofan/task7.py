1815671795e7e989bc9555834f94396332810f7e
Feofan228 <31817741+Feofan228@users.noreply.github.com>
2017-10-09 09:02:40 +0300

    Homework_2
    Тема домашнього завдання:"Поняття змінної та типу даних у Python"

---
+++ km73/Kazantsev_Feofan/task7.py
@@ -0,0 +1,6 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> n = int(input("Кількість хвилин,відрахованих після півночі:"))
+print("Години:",(n//60)%24)
+print("Хвилини:",n%60)
+k=int(input())
