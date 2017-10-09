bb46dceb393ec248746e5daeb696938cca454db5
Nastiaburova <31832759+Nastiaburova@users.noreply.github.com>
2017-10-09 09:13:47 +0300

    Homework_2

---
+++ km72/Burova_Anastasiya/task_1.py
@@ -0,0 +1,5 @@
+num1=float(input("number1= "))
+num2=float(input("number2= "))
+num3=float(input("number3= "))
+a=num1+num2+num3
+print(a)
---
+++ km72/Burova_Anastasiya/task_4.py
@@ -0,0 +1,6 @@
+N=int(input("Enter the number of student:"))
+K=int(input("Enter the number of apples:"))
+a=K//N
+b=K%N
+print("Every student gets ", a)
+print("Amount of apples that left is ", b)
