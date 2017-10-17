44fcfb137c60edd064f0114cda51d5651d5e0d62
Ann <32245955+annkucherova@users.noreply.github.com>
2017-10-17 18:16:04 +0300

    Delete task1.py

--- km72/Kucherova_Anna/task1.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Enter first  number: "))
-b = int(input("Enter second  number: "))
-
-if (a > b):
-    print("The smallest number is ",b)
-elif (a < b):
-    print("The smallest number is ",a)
-
