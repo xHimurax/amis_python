d3c10cb79dddc94974f5b7c8919c4f2b029fd279
Nastiaburova <31832759+Nastiaburova@users.noreply.github.com>
2017-10-29 23:50:02 +0200

    Delete task_6.py

--- km72/Burova_Anastasiya/task_6.py
+++
@@ -1,7 +0,0 @@
-G1=int(input("Amount of students in the first group= "))
-G2=int(input("Amount of students in the second group= "))
-G3=int(input("Amount of students in the third group= "))
-a=G1//2+G1%2
-b=G2//2+G2%2
-c=G3//2+G3%2
-print(a+b+c)
