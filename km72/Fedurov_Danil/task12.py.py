d6e26630286f2520aefbca528518e8942f925d49
Fedurov <31816537+Fedurov@users.noreply.github.com>
2017-10-29 20:18:34 +0300

    Delete task12.py.py

--- km72/Fedurov_Danil/task12.py.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter n:"))
-m = int(input("Enter m:"))
-k = int(input("Enter k:"))
-if n*m-k==n or n*m-k==m and n*m>k and k!=1:
-    print('YES')
-elif (n*m-k)%2==0 and n*m>k and k!=1:
-    print('YES')
-else:
-    print('NO')
