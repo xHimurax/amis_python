1126fce4e1b1cf11013b9812d2ad311cc753e9c2
Fedurov <31816537+Fedurov@users.noreply.github.com>
2017-10-29 22:21:35 +0300

    Add files via upload

---
+++ km72/Fedurov_Danil/4/task10.py
@@ -0,0 +1,14 @@
+x1 = int(input("Enter x1:"))
+x2 = int(input("Enter x2:"))
+y1 = int(input("Enter y1:"))
+y2 = int(input("Enter y2:"))
+
+if x1==y1 or x2==y2:
+    print("YES")
+elif (x1/y1)==(x2/y2):
+    print("YES")
+elif  (x1+x2)==(y1+y2)  or (x1-x2)==(y1-y2):
+    print("YES")   
+else:
+    print("NO")
+© 2017 GitHub, Inc.
