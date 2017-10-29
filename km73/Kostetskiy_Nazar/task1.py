f89b43fc892a9e5eb3d84a154aa9cc621e0198c0
NazarKostetskiy <31857245+NazarKostetskiy@users.noreply.github.com>
2017-10-29 22:10:24 +0200

    Delete km73/Kostetskiy_Nazar/task1.py

--- km73/Kostetskiy_Nazar/task1.py
+++
@@ -1,6 +0,0 @@
-a = float(input("Enter the first number:" ))
-b = float(input("Enter the second number:" ))
-c = float(input("Enter the third number:" ))
-s = a + b + c
-print("Total: ", s)
-input()
