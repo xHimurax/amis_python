1388bcf13e4761f3cc1c707d267de8d767889aad
NazarKostetskiy <31857245+NazarKostetskiy@users.noreply.github.com>
2017-10-29 22:10:33 +0200

    Delete km73/Kostetskiy_Nazar/task4.py

--- km73/Kostetskiy_Nazar/task4.py
+++
@@ -1,9 +0,0 @@
-N = int(input("Enter the number of students: "))
-
-K = int(input("Enter the number of apples: "))
-
-apples_per_student = K//N
-apples_in_bassket = K-(K//N)*N
-print("\nApples per student:",apples_per_student,"\nApples in the bassket:",apples_in_bassket)
-
-input()
