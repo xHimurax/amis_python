109d933e8e05ccddfd6adf2dd176691e0dbdc940
Feofan228 <31817741+Feofan228@users.noreply.github.com>
2017-10-30 00:52:36 +0200

    Delete task1.py

--- km73/Kazantsev_Feofan/task1.py
+++
@@ -1,9 +0,0 @@
-Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> a=int(input())
-n=int(input())
-if a<=n:
-    answer=a
-else :
-    answer=n
-print(answer)
