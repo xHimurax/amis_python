6ff1b686ca89d316d6c04ad6876c4443454d0c65
Andrew <31777699+bedermau5@users.noreply.github.com>
2017-10-29 14:50:28 +0300

    Added all new & old homework

---
+++ km72/Bederman_Andrey/Homework_5/Task3(new).py
@@ -0,0 +1,9 @@
+a = input().split()
+for i in range(len(a)):
+    for j in range(len(a)):
+        if i != j and a[i] == a[j]:
+            break
+    else:
+        print(a[i], end=' ')
+input("\n\nPress Enter to exit the program.")
+        
