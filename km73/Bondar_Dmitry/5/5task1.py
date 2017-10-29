94d0ae23a9f7a3dea37722791d37fae4d952774c
Dima Bondar <31803588+dmitrypm@users.noreply.github.com>
2017-10-29 19:22:16 +0200

    5task.1

---
+++ km73/Bondar_Dmitry/5/5task1.py
@@ -0,0 +1,9 @@
+hight = input("Введіть зріст людей в строю: ").split()
+PetyaHight=int(input("Введіть зріст Петі: "))
+answer = 1
+for i in range(len(hight)):
+    if int(hight[i]) >= PetyaHight:
+       answer +=1 
+        
+    
+print(answer)
