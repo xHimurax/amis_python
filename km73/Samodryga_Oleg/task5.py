612f7c14a8a387a88d26305a4ebc941d334baeeb
Jicei <31828588+Jicei@users.noreply.github.com>
2017-10-29 20:25:58 +0200

    Delete task5.py

--- km73/Samodryga_Oleg/task5.py
+++
@@ -1,5 +0,0 @@
-number=int(input("Введіть ціле число "))
-nex=int(number+1)
-prev=int(number-1)
-print("The next number for the number",(str)(number),"is",(str)(nex),"\nThe previous number for the number",(str)(number),"is",(str)(prev))
-input("")
