82de3e065c11b74edfd57fded941433433adf5d6
Dima Bondar <31803588+dmitrypm@users.noreply.github.com>
2017-10-29 19:53:25 +0200

    Delete task1.py

--- km73/Bondar_Dmitry/task1.py
+++
@@ -1,5 +0,0 @@
-﻿while True:
-  number1 = float(input("Введіть перше число:"))
-  number2 = float(input("Введіть друге число:"))
-  number3 = float(input("Введіть третє число:"))
-  print(number1+number2+number3)
