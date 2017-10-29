55cd1ad8716353b8b05b9634bece0a0ea7a8ce09
Dmytro2008 <31797651+Dmytro2008@users.noreply.github.com>
2017-10-29 22:46:28 +0200

    Add files via upload

---
+++ km72/Boroznuk_Dmytro/3/task2.py
@@ -0,0 +1,6 @@
+import time
+first_length=float(input("First cathetus: ")) #Перший катет
+second_length=float(input("Second cathetus: ")) #Другий катет
+area=((first_length*second_length)/2) 
+print("Area is", area)
+time.sleep(3)
