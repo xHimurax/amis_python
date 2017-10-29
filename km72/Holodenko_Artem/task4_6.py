0266b4898f214f90c645a0cbebc4c745e900b852
Holodenko_Artem <31826517+Acnologiak@users.noreply.github.com>
2017-10-29 19:09:45 +0200

    Delete task4_6.py

--- km72/Holodenko_Artem/task4_6.py
+++
@@ -1,8 +0,0 @@
-x=int(input("Введіть число x: "))
-y=int(input("Введіть число y: "))
-x1=int(input("Введіть число x1: "))
-y1=int(input("Введіть число y2: "))
-if x==x1 or y==y1:
-    print("YES")
-else:
-    print("NO")
