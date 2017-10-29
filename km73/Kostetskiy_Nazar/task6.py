fda182ec429578dadff7ea13c31b386a81a2b844
NazarKostetskiy <31857245+NazarKostetskiy@users.noreply.github.com>
2017-10-29 22:10:38 +0200

    Delete km73/Kostetskiy_Nazar/task6.py

--- km73/Kostetskiy_Nazar/task6.py
+++
@@ -1,7 +0,0 @@
-import math
-group_a = int(input('Enter the number of students A: '))
-group_b = int(input('Enter the number of students B: '))
-group_c = int(input('Enter the number of students C: '))
-tables = math.ceil((group_a + group_b + group_c)/2)
-print("\nYou have to buy",tables,"tables")
-input()
