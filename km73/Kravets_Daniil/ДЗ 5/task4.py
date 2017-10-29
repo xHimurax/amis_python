9101050f23518dc878993f70250c15273f88afec
davatar2000 <31825574+davatar2000@users.noreply.github.com>
2017-10-29 21:28:50 +0200

    Delete task4.py

--- km73/Kravets_Daniil/ДЗ 5/task4.py
+++
@@ -1,7 +0,0 @@
-a = [int(i) for i in input().split()]
-b = ['I'] * a[0]
-for i in range(a[1]):
-    c = [int(i) for i in input().split()]
-    for j in range(c[0], c[1] + 1):
-	    b[j - 1] = '.'
-print(''.join(b))
