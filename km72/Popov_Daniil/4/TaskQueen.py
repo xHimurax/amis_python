8521677ca42ae052faaceaa6cd08642e93652636
Daniil Popov <31805616+shkodder@users.noreply.github.com>
2017-10-29 13:17:27 +0200

    Add files via upload

---
+++ km72/Popov_Daniil/4/TaskQueen.py
@@ -0,0 +1,13 @@
+print('starting program')
+
+a1=int(input('enter 1st vertical'))
+b1=int(input('enter 1st horizontal'))
+a2=int(input('enter 2nd vertical'))
+b2=int(input('enter 2nd horizontal'))
+if a1<9 and a1>0 and b1<9 and b1>0 and a2<9 and a2>0 and b2<9 and b2>0:
+    if a1==a2 or b1==b2 or abs(a2-a1)==abs(b2-b1):
+        print('yes')
+    else:
+        print('no')
+else:
+    print('invalid coordinates')
