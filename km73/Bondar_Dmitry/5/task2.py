eaad4e15e0c274366ce2dc1e3c6b30ca6cb6c271
Dima Bondar <31803588+dmitrypm@users.noreply.github.com>
2017-10-30 02:45:28 +0200

    Delete task2.py

--- km73/Bondar_Dmitry/5/task2.py
+++
@@ -1,8 +0,0 @@
-x = input("Введіть список: ").split()
-b=[]
-for i in range(len(x)):
-    p = x.count(x[i])//2
-    if x[i] not in b:
-        for m in range( p):
-            b.append(x[i])
-print("Рівних між собою пар У списку - " , len(b))
