e1b96e90071c842edfaa980529d00a1805a4d0ca
Alex Gavrylenko <31793092+RyanKEK@users.noreply.github.com>
2017-10-30 18:35:21 +0200

    Delete Task4.3.py

--- km72/Gavrylenko_Olexandr/Task4/Task4.3.py
+++
@@ -1,9 +0,0 @@
-first_number = int(input('Enter 1st number:'))
-second_number = int(input('Enter 2nd number:'))
-third_number = int(input('Enter 3rd number:'))
-if first_number<second_number and third_number:
-    print(first_number)
-elif second_number<first_number and third_number:
-    print(second_number)
-elif third_number<first_number and second_number:
-    print(third_number)
