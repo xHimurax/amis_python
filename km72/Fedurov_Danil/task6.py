34a4bc182378007655a18702bbff53c63a14ce6b
Fedurov <31816537+Fedurov@users.noreply.github.com>
2017-10-30 22:19:45 +0300

    Delete task6.py

--- km72/Fedurov_Danil/task6.py
+++
@@ -1,9 +0,0 @@
-x = int(input("первой группе:"))
-y = int(input("второй группе:"))
-z = int(input("третьей группе:"))
-sum = x + y + z
-tableOn3Groups = sum // 2
-if sum % 2 == 1:
-    tableOn3Groups = 1
-        
-print("Мінімальну кількість столів необхідно придбати:",tableOn3Groups)
