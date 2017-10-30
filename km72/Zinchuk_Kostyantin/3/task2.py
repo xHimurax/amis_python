4e06f1daaa6482c00377a4956251bf3b271b8941
kostyazinchuk <31826585+kostyazinchuk@users.noreply.github.com>
2017-10-30 03:54:16 +0300

    Add files via upload

---
+++ km72/Zinchuk_Kostyantin/3/task2.py
@@ -0,0 +1,3 @@
+x=float(input(" Enter lenght of X "))
+y=float(input(" Enter lenght of Y "))
+print("SQUARE= ", x*y*0.5)
---
+++ km72/Zinchuk_Kostyantin/3/task5.py
@@ -0,0 +1,2 @@
+x=int(input("enter X: "))
+print("previous number" ,x-1,"\nnext number",x+1)
