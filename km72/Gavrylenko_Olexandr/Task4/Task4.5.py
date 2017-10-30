304560794e1ae09bf43bd2aec9b44657c382548e
Alex Gavrylenko <31793092+RyanKEK@users.noreply.github.com>
2017-10-30 18:35:32 +0200

    Delete Task4.5.py

--- km72/Gavrylenko_Olexandr/Task4/Task4.5.py
+++
@@ -1,9 +0,0 @@
-first_number = int(input('Enter 1st number:'))
-second_number = int(input('Enter 2nd number:'))
-third_number = int(input('Enter 3rd number:'))
-if first_number == second_number == third_number:
-    print(3)
-elif first_number == second_number or second_number == third_number or first_number == third_number:
-    print(2)
-else:
-    print(0)
