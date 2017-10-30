502e22696e9821a93c94cd962738994a56ad23f1
Fedurov <31816537+Fedurov@users.noreply.github.com>
2017-10-30 22:19:33 +0300

    Delete task4.py

--- km72/Fedurov_Danil/task4.py
+++
@@ -1,7 +0,0 @@
-N = int(input("студентов:"))
-K = int(input("яблук:"))
-
-appleForStudent = K // N
-appleInBag = K % N
-print("Сколько яблок на студента",appleForStudent)
-print("Сколько яблок в ящике",appleInBag)  
